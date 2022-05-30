4145881f9a7f36484e101f2199362c26241b7f11
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2022-05-30 14:55:43 +0200

    setup for save system

---
+++ Soulmates (URP)/Assets/Dinand Programming/Level 3 scripts/SoulmateDesingData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Desing Data", menuName = "ScriptableObjects/Data/Design Data")]
+public class SoulmateDesingData : ScriptableObject
+{
+    public GameObject Shape;
+}
