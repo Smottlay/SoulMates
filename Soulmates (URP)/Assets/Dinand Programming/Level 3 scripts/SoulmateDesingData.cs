29c5f5558a9afbf6987d682323fd3b701c99d7dc
JustFunPlay <78020014+JustFunPlay@users.noreply.github.com>
2022-05-31 16:59:19 +0200

    save data setup (again)

--- Soulmates (URP)/Assets/Dinand Programming/Level 3 scripts/SoulmateDesingData.cs
+++ Soulmates (URP)/Assets/Dinand Programming/Level 3 scripts/SoulmateDesingData.cs
@@ -5,5 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Desing Data", menuName = "ScriptableObjects/Data/Design Data")]
 public class SoulmateDesingData : ScriptableObject
 {
-    public GameObject Shape;
+    public int shapeIndex;
+    public int floorTextureIndex;
+    public int wallTextrureIndex;
+    public int ceilingTextureIndex;
+    public List<PropInfo> windows = new List<PropInfo>();
+    public List<PropInfo> props = new List<PropInfo>();
+}
+
+[System.Serializable]
+public class PropInfo
+{
+    public int propIndex;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public PropInfo(int index, Vector3 pos, Quaternion rot)
+    {
+        this.propIndex = index;
+        this.position = pos;
+        this.rotation = rot;
+    }
 }
