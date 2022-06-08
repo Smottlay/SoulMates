196a86d99b6ae3b6e799cb22c8299057d07195e2
arthur-lava <97080076@st.deltion.nl>
2022-06-08 16:44:33 +0200

    plaatleters geinplementeert

--- Soulmates (URP)/Assets/Arthurs Folder/Void level/PlaatsLetters.cs
+++ Soulmates (URP)/Assets/Arthurs Folder/Void level/PlaatsLetters.cs
@@ -14,9 +14,13 @@ public class PlaatsLetters : MonoBehaviour
             trigger = other.gameObject;
         }
     }
+    public void PickUpLetter() {
+        this.GetComponent<Rigidbody>().isKinematic = false;
+    }
     public void DropLetter() {
         // maak het zo dat als je hem los laat en hij zit niet op de juiste plek, dat hij dan weer terug gaat naar de orginele locatie en weer om je heen draait
         // zodat je hem weer opnieuw kan pakken:) veel succes mfer, beter ga je deze shit snel af maken anders krijg je izjem klap op je bek
-        this.GetComponent<RotateAround>().enabled = false;
+        //this.GetComponent<RotateAround>().enabled = false;
+        this.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
