f3f71f08645248b751a8c1435c0a8df93abb9e26
arthur-lava <97080076@st.deltion.nl>
2022-06-08 16:59:09 +0200

    Update PlaatsLetters.cs

--- Soulmates (URP)/Assets/Arthurs Folder/Void level/PlaatsLetters.cs
+++ Soulmates (URP)/Assets/Arthurs Folder/Void level/PlaatsLetters.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlaatsLetters : MonoBehaviour
 {
     private GameObject trigger;
+    private Vector3 rotat;
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag(this.gameObject.name)) {
             this.transform.position = other.transform.position;
@@ -16,11 +17,13 @@ public class PlaatsLetters : MonoBehaviour
     }
     public void PickUpLetter() {
         this.GetComponent<Rigidbody>().isKinematic = false;
+        this.GetComponent<Rigidbody>().rotation = new Quaternion(0, 90, 0, 0);
     }
     public void DropLetter() {
         // maak het zo dat als je hem los laat en hij zit niet op de juiste plek, dat hij dan weer terug gaat naar de orginele locatie en weer om je heen draait
         // zodat je hem weer opnieuw kan pakken:) veel succes mfer, beter ga je deze shit snel af maken anders krijg je izjem klap op je bek
         //this.GetComponent<RotateAround>().enabled = false;
         this.GetComponent<Rigidbody>().isKinematic = true;
+        this.GetComponent<Rigidbody>().rotation = new Quaternion(0, 0, 0, 0);
     }
 }
