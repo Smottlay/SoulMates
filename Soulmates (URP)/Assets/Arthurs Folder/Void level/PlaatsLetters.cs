fa89822ddb959d47d63e3e1c8dcbb7c55ce7e406
arthur-lava <97080076@st.deltion.nl>
2022-06-28 17:13:28 +0200

    trasition bijna af

--- Soulmates (URP)/Assets/Arthurs Folder/Void level/PlaatsLetters.cs
+++ Soulmates (URP)/Assets/Arthurs Folder/Void level/PlaatsLetters.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlaatsLetters : MonoBehaviour
 {
-    private GameObject trigger;
-    private Vector3 rotat;
-    private bool laatsteLetter;
+    public GameObject LettersWithShader;
+    public GameObject Letters;
+    public Renderer disolveShader;
+    float dissolve;
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.name == this.gameObject.name) {
             Color newcolor = other.GetComponent<MeshRenderer>().material.color;
@@ -15,8 +16,14 @@ public class PlaatsLetters : MonoBehaviour
             Destroy(this.gameObject);
             if (this.transform.parent.childCount <= 1) {
                 Debug.Log("Laatste letter is geplaatst");
+                LettersWithShader.SetActive(true);
+                Letters.SetActive(false);
+                dissolve = 5f;
+                for (int i = 0; i < 10; i++) {
+                    
+                    disolveShader.material.SetFloat("DissolveRange", dissolve);
+                }
             }
         }
-
     }
 }
