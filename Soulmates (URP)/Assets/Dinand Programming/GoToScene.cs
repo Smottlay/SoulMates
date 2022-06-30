ae740d5990ed49f94ff8fc2ecc3c4668ba512d34
Smottlay <53128377+Smottlay@users.noreply.github.com>
2022-06-30 14:32:52 +0200

    Updated More UI

--- Soulmates (URP)/Assets/Dinand Programming/GoToScene.cs
+++ Soulmates (URP)/Assets/Dinand Programming/GoToScene.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GoToScene : MonoBehaviour
 {
     public int newScene;
+    public GameObject[] panels;
 
     public void SwithScene()
     {
         SceneManager.LoadScene(newScene);
+    }
+
+    public void SwitchPanel()
+    {
+        panels[0].SetActive(true);
+        panels[1].SetActive(false);
     }
 }
